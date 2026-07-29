558c30963d8862915b8d454ed5b2bc968314a20d
justinarsey01 <125629259+justinarsey01@users.noreply.github.com>
2026-07-29 20:22:17 +0100

    2nd install button

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', (event) => {
+const CACHE = 'apexvolt-v1';
+
+self.addEventListener('install', e => {
   self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
